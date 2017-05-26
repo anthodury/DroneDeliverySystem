31601749ebce66d57e3deeafc019fab164598e08
anthodury <anthony.dury.5@gmail.com>
2017-05-26 16:03:15 +0200

    Created Thread for Weather. Updated CMakeList Consequently

--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void *changeWeather(void *data)
+{
+    while(1){
+        // Change weather
+        // Notify drone and others ...
+        printf("Weather changed\n");
+        unsigned int timeWaiting=3+rand()%30;
+        sleep(timeWaiting);
+    }
+}
 
 int main() {
 	printf("Hello, World!\n");
+
+    pthread_t threadWeather;
+	if (pthread_create(&threadWeather, 0, changeWeather,NULL)){
+		printf("error creating thread weather");
+	}
+    //pthread_kill(threadWeather, SIGKILL);// Kill thread weather
+    pthread_exit(NULL);
+    
 	return 0;
 }
